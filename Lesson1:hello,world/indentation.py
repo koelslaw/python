ec576456044889b23a56e6bd5bbac8941a223fef
Kirk K <koelslaw@gmail.com>
2022-01-23 12:15:16 -0600

    add dirs and started lesson 2

---
+++ Lesson1:hello,world/indentation.py
@@ -0,0 +1,4 @@
+x = 1
+if x == 1:
+    #code editor took care of it for me, so this is just to test it out!!!
+    print("x is 1.")
--- indentation.py
+++
@@ -1,4 +0,0 @@
-x = 1
-if x == 1:
-    #code editor took care of it for me, so this sis just to test it out!!!
-    print("x is 1.")
