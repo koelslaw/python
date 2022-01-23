c3b7da5c6b3171acbaa564a09168eda3b673d33c
Kirk K <koelslaw@gmail.com>
2022-01-23 13:38:30 -0600

    Create operators.py

---
+++ Lesson2:VariablesAndTypes/operators.py
@@ -0,0 +1,13 @@
+variableone = 1
+variabletwo = 4
+variableanswer= variabletwo + variableone
+print (variableanswer)
+
+stringone = "koelslaw"
+
+stringtwo = "is awesome"
+
+fullstring = stringone + " " + stringtwo + "!!!"
+
+print(fullstring)
+
