266c63c6eebc76646a2e4c5696749c55bc477d8b
Kirk K <koelslaw@gmail.com>
2022-01-23 13:30:17 -0600

    Create strings.py

---
+++ Lesson2:VariablesAndTypes/strings.py
@@ -0,0 +1,7 @@
+mystring = 'some random string'
+print(mystring)
+mystring = "some other randome string"
+print(mystring)
+##Single quotes wont allow apostophes
+mystring = "Don't worry about that apostrophe"
+print(mystring)
